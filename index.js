105271d55d2625333b7f901d5d7ad66d89c6829b
Shesha Sai Seetha <shesha.usms@gmail.com>
2023-03-04 14:49:51 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+
+var randomDiscImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiscImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomDiscImage2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomDiscImage2);
+
+if (randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+else if (randomNumber1 < randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
+else
+{
+    document.querySelector("h1").innerHTML = "Draw";
+}
+
+
+
